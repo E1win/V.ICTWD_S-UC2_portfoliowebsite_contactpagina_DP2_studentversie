1a2f8e483cbc1bc49e3d07bafff9d61231ba5953
E1win <87700553+E1win@users.noreply.github.com>
2026-03-06 14:07:46 +0100

    fixed email regex

--- Portfoliowebsite/Models/ContactModel.cs
+++ Portfoliowebsite/Models/ContactModel.cs
@@ -14,7 +14,7 @@ namespace Portfoliowebsite.Models
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Vul uw emailadres in.")]
         [StringLength(255, ErrorMessage = "Maximaal {1} tekens.")]
-        [RegularExpression("/^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$/", ErrorMessage = "Vul een geldig emailadres in.")]
+        [RegularExpression("(?:[a-z0-9!#$%&'*+\\x2f=?^_`\\x7b-\\x7d~\\x2d]+(?:\\.[a-z0-9!#$%&'*+\\x2f=?^_`\\x7b-\\x7d~\\x2d]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9\\x2d]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9\\x2d]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\\x2d]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", ErrorMessage = "Vul een geldig emailadres in.")]
         public string? Email { get; set; }
 
         [Display(Name = "Onderwerp")]
